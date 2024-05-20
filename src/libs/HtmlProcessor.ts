66dcc8c325fbd885a6cfdb32b93d0e1e18c5b3f4
Max P <Mail@MPassarello.de>
2024-05-20 15:58:33 +0200

    Subject: Enhanced HTML processing for epub validation
    This update provides an extension to the HtmlProcessor class functionalities to improve its processing capabilities while complying with epub validations. Refinements include selectively skipping specific tags and tags with certain classes from bold text formatting, thus reducing unexpected layout reshapings.
    
    The parser setup is also adjusted, with 'decodeEntities' turned on and 'xmlMode' set to false during the read operation, but reversed during the write operation. This setup aids in correctly parsing and re-serializing the document, enhancing readability and conformity to epub standards.
    
    The code for creating array of bold nodes underwent improvements as well. The regular expression now properly handles Unicode characters, resulting in a more precise bold formatting.
    
    These changes assure an enriched user reading experience and improved consistency across diverse formats and devices, ensuring our product's continued reliability and accessibility.

--- src/libs/HtmlProcessor.ts
+++ src/libs/HtmlProcessor.ts
@@ -1,34 +1,63 @@
 import { Parser } from 'htmlparser2';
-import { DomHandler, Element, Text, Node, ChildNode } from 'domhandler';
+import { DomHandler, Element, Text, ChildNode } from 'domhandler';
 import { default as serialize } from 'dom-serializer';
 
 /**
  * Class responsible for processing HTML content.
  */
 export class HtmlProcessor {
+    private excludedTags = [
+        'a',
+        'meta',
+        'title',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'link',
+        'script',
+        'style',
+    ];
+    private excludedTagsWithClasses: { [tag: string]: string[] } = {
+        p: ['caption', 'parttext'],
+        span: ['bold'],
+        div: ['listing'],
+    };
+
     /**
      * Processes the given HTML content by parsing it, applying modifications, and serializing it back to HTML.
      * @param html - The HTML content to process.
      * @returns The processed HTML content.
      */
     process(html: string): string {
-        const handler = new DomHandler();
-        const parser = new Parser(handler);
+        const handler = new DomHandler(undefined, {
+            withStartIndices: false,
+            withEndIndices: false,
+        });
+        // For epub validation, we need to set xmlMode to false on read
+        const parser = new Parser(handler, {
+            decodeEntities: true,
+            xmlMode: false,
+        });
         parser.write(html);
         parser.end();
 
         const document = handler.dom;
         this.boldText(document);
 
+        // For epub validation, we need to set xmlMode to true on write
         const serializedDocument = serialize(document, {
-            decodeEntities: false,
+            decodeEntities: true,
             xmlMode: true,
         });
         return serializedDocument;
     }
 
     /**
-     * Recursively applies bold formatting to text nodes within the given array of child nodes.
+     * Recursively applies bold formatting to text nodes within the given array of child nodes,
+     * but skips specific tags and tags with specific classes.
      * @param nodes - The array of child nodes to process.
      */
     private boldText(nodes: ChildNode[]) {
@@ -45,6 +74,21 @@ export class HtmlProcessor {
                 }
             } else if (node.type === 'tag') {
                 const elementNode = node as Element;
+                const tagName = elementNode.name;
+                const classList = elementNode.attribs?.class?.split(' ') || [];
+
+                // Skip the entire subtree if the current element is excluded
+                if (
+                    this.excludedTags.includes(tagName) ||
+                    (tagName in this.excludedTagsWithClasses &&
+                        classList.some((cls) =>
+                            this.excludedTagsWithClasses[tagName].includes(cls)
+                        ))
+                ) {
+                    return;
+                }
+
+                // Process child nodes recursively
                 if (elementNode.children) {
                     this.boldText(elementNode.children);
                 }
@@ -54,16 +98,17 @@ export class HtmlProcessor {
 
     /**
      * Creates an array of child nodes with bold formatting based on the given text.
+     * Handles Unicode characters correctly.
      * @param text - The text to format.
      * @returns An array of child nodes with bold formatting.
      */
     private createBoldNodes(text: string): ChildNode[] {
-        const parts = text.match(/\w+|[^\s\w]+|\s+/g) || [];
+        const parts = text.match(/[\p{L}\p{N}]+|[^\s\p{L}\p{N}]+|\s+/gu) || [];
         const nodes: ChildNode[] = [];
         parts.forEach((part) => {
-            if (part.match(/\s+/)) {
+            if (part.match(/\s+/u)) {
                 nodes.push(new Text(part));
-            } else if (part.match(/[^\w]/)) {
+            } else if (part.match(/[^\p{L}\p{N}]/u)) {
                 nodes.push(new Text(part));
             } else if (part.length <= 3) {
                 const boldElement = new Element('b', {});
