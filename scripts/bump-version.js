1c3a5acbf1098d3d437ee28b6008d2c1948a046f
Max P <Mail@MPassarello.de>
2024-05-20 18:56:36 +0200

    Update version property to be read-only in bump-version script
    In the version bumping script, the `version` property in `Config.ts` has been set to read-only, preventing unintended modifications throughout the codebase. The regex used to replace the version has been updated to account for this change. Following this update, the new version will be set correctly while ensuring data integrity by preventing accidental modifications to the version number.

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -58,7 +58,7 @@ console.log(`Version updated to ${packageJson.version}`);
 
 // Update the version number in Config.ts
 let configTsContent = fs.readFileSync(configTsPath, 'utf8');
-const versionRegex = /public static version = ['"](\d+\.\d+\.\d+)['"]/;
+const versionRegex = /public static readonly version = ['"](\d+\.\d+\.\d+)['"]/;
 configTsContent = configTsContent.replace(versionRegex, `public static version = '${newVersion}'`);
 
 // Write the updated Config.ts back to the file system
